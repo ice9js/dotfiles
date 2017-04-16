aea63199ec1b77010f8e6f3d5d817b6e1423c19c
Kuba Birecki <kuba.birecki@crystalplanet.io>
2017-04-16 18:20:35 +0200

    Add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This scripts configures the machine by running
+# all scripts from the install/ directory.
+
+./install/brew
+./install/terminal
+./install/vim
+./install/git
+./install/sublime
+./install/node
