97be8d8538bde8d7fc73d4035591693532933567
Kuba Birecki <kuba.birecki@crystalplanet.io>
2017-04-16 17:20:11 +0200

    Add git configuration

---
+++ install/git.sh
@@ -0,0 +1,7 @@
+##!/bin/bash
+
+# Git comes pre-installed as part of Command Line Tools.
+# This file only manages Git configuration.
+
+ln -s $(pwd)/.gitignore ~/.gitignore
+ln -s $(pwd)/.gitconfig ~/.gitconfig
