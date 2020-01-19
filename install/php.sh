e9f81f5558f9300ecfef13a21560f330718536fc
Kuba Birecki <kuba.birecki@crystalplanet.io>
2020-01-19 22:40:02 +0100

    Update PHP installation script

--- install/php.sh
+++ install/php.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Install php and dupes repositories
-brew tap homebrew/dupes
-brew tap homebrew/php
-
-# Install PHP 7.2
-brew install php72
+# Install latest PHP version
+brew install php
 
 # Install composer
 brew install composer
